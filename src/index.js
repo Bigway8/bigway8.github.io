9b4d1ed496c2c9eb80a88f53166fed5e5d82aa6a
David Bigway <50246223+Bigway8@users.noreply.github.com>
2022-11-23 16:22:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,24 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 )
-
-
-
-const TELEGRAM_URI = `https://api.telegram.org/bot5917789698:AAEDrfFN2HAqte2A2hj59RNnwl-99oWYZdk/`
-const message =
-  '🤬Пора сдавать ДТП \n @revindenis @AliceKoti @LNAG2 @AndreyKudri'
-const my_id = 440192765
-const chat_id = -1001792969931
-
-async function getMe(url) {
-  await fetch(url + `sendMessage?chat_id=${my_id}&text=${message}`, {
-    method: 'post',
-  })
-}
-setInterval(() => {
-  const time = `${new Date().getHours()}:${new Date().getMinutes()}`
-  getMe(TELEGRAM_URI)
-  if (time == '18:00') {
-    getMe(TELEGRAM_URI)
-  }
-}, 5 * 1000)
