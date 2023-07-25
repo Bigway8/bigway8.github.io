3f4da6340c00b493ace03707fc7d2671ec99ce1c
David Bigway <50246223+Bigway8@users.noreply.github.com>
2023-07-25 13:50:22 +0300

    Update tg.js

--- static/js/tg.js
+++ static/js/tg.js
@@ -1,21 +1,3 @@
-const TELEGRAM_URI = `https://api.telegram.org/bot5917789698:AAEDrfFN2HAqte2A2hj59RNnwl-99oWYZdk/`
-const message =
-  '🤬Пора сдавать ДТП'
-const chat_id = -1001792969931
-
-async function getMe(url) {
-  await fetch(url + `sendMessage?chat_id=${chat_id}&text=${message}`, {
-    method: 'post',
-  })
-}
-setInterval(() => {
-  const time = `${new Date().getHours()}:${
-    new Date().getMinutes() < 10
-      ? '0' + new Date().getMinutes()
-      : new Date().getMinutes()
-  }`
-  console.log(time)
-  if (time == '11:30') {
-    getMe(TELEGRAM_URI)
-  }
-}, 60 * 1000)
+function getMe(url) {
+return null
+})
